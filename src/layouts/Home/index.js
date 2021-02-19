6f80cc7c04bde0bcd76cd4e9f4d46c965feaec65
Murphy Hsieh <g110834005@grad.ntue.edu.tw>
2021-02-19 09:57:37 +0800

    add components folder and example component

--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Header from '../../components/Header';
+
 import styles from './styles.module.scss';
 
 const Home = () => {
-  return <div className={styles.container}>This is Home page</div>;
+  return (
+    <div className={styles.container}>
+      <Header />
+      This is Home page
+    </div>
+  );
 };
 
 export default Home;
